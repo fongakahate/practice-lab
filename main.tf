6db2fd7a0fb5b8d3d2bcccd3dc9fec510a21cf67
Volodymyr Hilevskyi <fongakahate@gmail.com>
2021-09-21 17:19:12 +0300

    123

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ resource "aws_security_group" "PublicEC2SG" {
 }
 
 resource "aws_instance" "test" {
-  ami           = "ami-0e7ecbee23c8f8a4b"
+  ami = ""
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.PublicEC2SG.id]
   tags = {
